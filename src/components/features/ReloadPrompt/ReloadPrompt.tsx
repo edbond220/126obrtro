14b3e1f127e5c1d9b2240cc28628f9714e194dbe
edbond <edbond88@gmail.com>
2022-09-20 14:58:15 +0300

    feat: add prompt to reload SW

---
+++ src/components/features/ReloadPrompt/ReloadPrompt.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useRegisterSW } from 'virtual:pwa-register/react';
+
+import './reloadPrompt.css';
+
+function ReloadPrompt() {
+  const {
+    offlineReady: [offlineReady, setOfflineReady],
+    needRefresh: [needRefresh, setNeedRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegistered(r) {
+      console.log('SW Registered: ' + r);
+    },
+    onRegisterError(error) {
+      console.log('SW registration error', error);
+    },
+  });
+
+  const close = () => {
+    setOfflineReady(false);
+    setNeedRefresh(false);
+  };
+
+  return (
+    <div className="reloadPrompt-container">
+      {(offlineReady || needRefresh) && (
+        <div className="reloadPrompt-toast">
+          <div className="reloadPrompt-message">
+            {offlineReady ? (
+              <span>App ready to work offline</span>
+            ) : (
+              <span>New content available, click on reload button to update.</span>
+            )}
+          </div>
+          {needRefresh && (
+            <button className="reloadPrompt-toast-button" onClick={() => updateServiceWorker(true)}>
+              Reload
+            </button>
+          )}
+          <button className="reloadPrompt-toast-button" onClick={() => close()}>
+            Close
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ReloadPrompt;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+// import { registerSW } from 'virtual:pwa-register';
+import ReloadPrompt from '@components/ReloadPrompt/ReloadPrompt';
+
 import Router from '@/Router';
+
 import './styles/global.css';
+
+const intervalMS = 60 * 60 * 1000;
+//
+// const updateSW = registerSW({
+//   onRegisteredSW(swUrl, r) {
+//     r &&
+//       setInterval(async () => {
+//         if (!(!r.installing && navigator)) return;
+//
+//         if ('connection' in navigator && !navigator.onLine) return;
+//
+//         const resp = await fetch(swUrl, {
+//           cache: 'no-store',
+//           'cache-control': 'no-cache',
+//         });
+//
+//         if (resp?.status === 200) await r.update();
+//       }, intervalMS);
+//   },
+// });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+    <ReloadPrompt />
   </React.StrictMode>
 );
